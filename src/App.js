f1fef13a141a90b9d27f3154813e7ec7cc72a24b
mahin-wd <wdmahin.mhm@gmail.com>
2022-10-10 14:51:50 +0600

    navigation bar and router setup finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layouts/Main';
+import Home from './Components/Home/Home';
+import Statistic from './Components/Statistics/Statistic';
+import Blog from './Components/Blog/Blog'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/statistic',
+          element: <Statistic></Statistic>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Navigation-Bar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <div className="nav-bar">
+            <div className="nav-name">
+                <h2>Quiz Hut</h2>
+            </div>
+
+            <div className="nav-item">
+                <nav>
+                    <Link to ='/'>Home</Link>
+                    <Link to ='/statistic'>Statistic</Link>
+                    <Link to ='/blog'>Blog</Link>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
